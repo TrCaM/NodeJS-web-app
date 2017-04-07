59cd14c6ed3bbdccfa46b4e03462f3ecafc14580
TrCaM <TriCao@cmail.carleton.ca>
2017-04-07 01:20:50 -0400

    First one

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+
+const ROOT = "./public";
+
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+app.use(function(req, res, next){
+    console.log(req.method+ " request for "+ req.url);
+    next();
+});
+
+app.get("/", function(req,res){
+    res.render("index");
+});
+
+app.use(express.static(ROOT));
+
+app.all("*", function(req,res){
+    res.sendStatus(404);
+});
+app.listen(1996, function(){console.log("Server listening on port 1996")});
